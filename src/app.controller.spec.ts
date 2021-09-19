0d069d34b5825f3779b39dfcc874551a73c30753
alcance <alcance.enlinea@gmail.com>
2021-09-19 09:25:20 -0700

    :white_check_mark: first test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "🤖 Hello human."', () => {
+      expect(appController.getHello()).toBe('🤖 Hello human.');
     });
   });
 });
